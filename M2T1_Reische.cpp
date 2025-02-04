814f1a139c2b9f9cca36ff738b2a4767a1a6889b
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-04 17:34:10 +0000

    Changed around some of the code and added iomanip so i could setprecision for the final price.

---
+++ M2T1_Reische.cpp
@@ -0,0 +1,23 @@
+//CSC 134
+//M2T1
+//Dalton Reische
+//2/4/2025
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+//Give all the variables
+    string name = "Dalton Reische";
+    int apples = 100;
+    double pricePerApple = 0.25;
+// Calculate the total price of the apples
+    double totalPrice = apples * pricePerApple;
+// Print all the information about the apple farm
+    cout << "Welcome to " << name << "'s apple farm!\n";
+    cout << "We have " << apples << " apples in stock\n";
+    cout << "Apples are currently $" << pricePerApple << " each.\n";
+    cout << fixed << setprecision(2);
+    cout << "If you want them all, the final price will be $" << totalPrice <<endl;
+
+    return 0;
+}
