3f8c7f4843343af3801123b054e235b2d97a1d25
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-11 17:48:41 +0000

    Added the code to display the calculated numbers to the user

--- M2LAB1_Reische.cpp
+++ M2LAB1_Reische.cpp
@@ -34,4 +34,11 @@ int main() {
     cost = volume * COST_PER_CUBIC_FOOT;
     charge = volume * CHARGE_PER_CUBIC_FOOT;
     profit = charge - cost;
+//Display the calculated data
+    cout << "The volume of the crate is " << volume << " cubic feet.\n";
+    cout << "Cost to build: $" << cost << endl;
+    cout << "Charge to customer: $" << charge << endl;
+    cout << "Profit: $" << profit << endl;
+
+return 0;
 }
