986b2a3f77d2ec5da1ca436309c5ab6f1f905495
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-11 17:19:49 +0000

    Added the constant values

--- M2LAB1_Reische.cpp
+++ M2LAB1_Reische.cpp
@@ -2,3 +2,14 @@
 //M2LAB1
 //Dalton Reische
 //2/11/2025
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+//Give constants
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+
+}
