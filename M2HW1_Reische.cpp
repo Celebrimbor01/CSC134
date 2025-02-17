fe320266d2b6d7112615511b690c5d7f615d2b37
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-17 12:34:50 +0000

    Added a few missing variables and the code needed to calculate the final balance after the deposit and withdrawal. Added the code to display the accounts information and fixed the account number constant.

--- M2HW1_Reische.cpp
+++ M2HW1_Reische.cpp
@@ -11,13 +11,15 @@ int main() {
 //Question 1: This program will simulate banking transactions.
 
 //Constants
-    const int account_number = 6973990106;
+    const long int account_number = 7355990106;
 
 //Variables
     string name;
     double starting_balance;
     double deposit_amount;
+    double new_balance;
     double withdrawal_amount;
+    double final_balance;
 
 //Give this problem the "Question 1" label.
     cout << "Question 1.\n";
@@ -32,5 +34,17 @@ int main() {
     cin >> deposit_amount;
     cout << "Please enter the amount you wish to withdrawal: $";
     cin >> withdrawal_amount;
+
+//Calculate the final account balance
+    new_balance = starting_balance + deposit_amount;
+    final_balance = new_balance - withdrawal_amount;
+
+//Print all the information about the users bank account.
+    cout << "\n";
+    cout << "Name on the account: " << name << endl;
+    cout << "Account number: " << account_number << endl;
+    cout << "Account balance after withdrawal: $" << final_balance << endl;
+
+return 0;
 
 }
