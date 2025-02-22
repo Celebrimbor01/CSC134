efd6453c1cd2b761e71af35ac49b5b78fd07ee82
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-22 11:38:53 +0000

    Started M3T1. This program will get the length and width of two rectangles and display the area of both.

---
+++ M3T1_Reische.cpp
@@ -0,0 +1,16 @@
+//CSC 134
+//M3T1
+//Dalton Reische
+//2/22/2025
+
+//This program will get the length and width of two rectangles and display the area of both.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+
+
+}
