7f98c24dd3995c5b1ff344f6803a936bf0d21ebf
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-22 11:51:51 +0000

    Added the code to prompt the user for the length and width of both rectangles and stored the responses in variables. Condensed the variables list as well.

--- M3T1_Reische.cpp
+++ M3T1_Reische.cpp
@@ -11,6 +11,15 @@ using namespace std;
 
 int main() {
 
+//Variables
+    double length_one, width_one;
+    double area_one;
+    double length_two, width_two;
+    double area_two;
 
-
+//Promp the user for the length and width of both rectangles.
+    cout << "Please enter the length and width of the first rectangle (in feet), seperated by a space: ";
+    cin >> length_one >> width_one;
+    cout << "Please enter the length and width of the second rectangle (in feet), seperated by a space: ";
+    cin >> length_two >> width_two;
 }
