2340e410ce81a41c89fbd6571f596bbd1844e712
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-22 12:12:41 +0000

    Started M3T2

---
+++ M3T2_Reische.cpp
@@ -0,0 +1,16 @@
+//CSC 134
+//M3T2
+//Dalton Reische
+//2/22/2025
+
+//This program will calculate the area of two rectangles and display which has the larger area or if they are equal.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+
+
+}
