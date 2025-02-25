7f4467978d11d0fa0460bcc4735ba8413a1a8fab
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-25 17:44:43 +0000

    Fixed a comment

--- M3LAB1_Reische.cpp
+++ M3LAB1_Reische.cpp
@@ -3,7 +3,7 @@
 //Dalton Reische
 //2/25/2025
 
-//This program will simulate a very drag race, where the user makes a choice and then finds out if they've won.
+//This program will simulate a drag race, where the user makes a choice and then finds out if they've won.
 
 #include <iostream>
 #include <string>
