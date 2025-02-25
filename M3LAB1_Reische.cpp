4adfb0fa3b9dc1288f7f863b261bd81c3194d677
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-25 17:10:02 +0000

    Started M3LAB1

---
+++ M3LAB1_Reische.cpp
@@ -0,0 +1,10 @@
+//CSC 134
+//M3LAB1
+//Dalton Reische
+//2/25/2025
+
+//This program will simulate a very drag race, where the user makes a choice and then finds out if they've won.
+
+#include <iostream>
+#include <string>
+using namespace std;
