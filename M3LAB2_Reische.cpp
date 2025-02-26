99b5b506f482e6b620e9a5ed2332c4cd3cb05f40
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-26 17:01:32 +0000

    Added the code to convert the numerical grade into a letter grade.

--- M3LAB2_Reische.cpp
+++ M3LAB2_Reische.cpp
@@ -20,6 +20,25 @@ using namespace std;
     cin >> numericalGrade;
 
 //Convert the numerical grade into a letter grade.
-    
+    if (numericalGrade >= 90 && numericalGrade <= 100) {
+        cout << "\nYour letter grade is: A\n";
+    }
+    else if (numericalGrade >= 80 && numericalGrade <= 89) {
+        cout << "\nYour letter grade is: B\n";
+    }
+    else if (numericalGrade >= 70 && numericalGrade <= 79) {
+        cout << "\nYour letter grade is: C\n";
+    }
+    else if (numericalGrade >= 60 && numericalGrade <= 69) {
+        cout << "\nYour letter grade is: D\n";
+    }
+    else if (numericalGrade >= 0 && numericalGrade <= 59) {
+        cout << "\nYour letter grade is: F\n";
+    }
+    else {
+        cout << "\nInvalid grade entered.\n";
+    }
+
+return 0;
 
 }
