5720a9d366479c23d827beff3f6db996314d07a1
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-02-26 16:40:36 +0000

    Started M3LAB2

---
+++ M3LAB2_Reische.cpp
@@ -0,0 +1,18 @@
+//CSC 134
+//M3LAB2
+//Dalton Reische
+//2/26/2025
+
+//This program will convert numerical grades to letter grades.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Begin main function.
+    int main() {
+
+//Variables
+
+
+}
