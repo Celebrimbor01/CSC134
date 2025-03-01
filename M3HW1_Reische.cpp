7743269875314f2a2afab0b8d81445c17f4413ab
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-03-01 12:04:30 +0000

    Added the variables and code to lable question 1 when displaying. Added the code to prompt the user and store the answer in the variable. Added the code to determine what the chat bots response will be and display the response to the user.

--- M3HW1_Reische.cpp
+++ M3HW1_Reische.cpp
@@ -8,8 +8,30 @@
 using namespace std;
 
 int main() {
+//Question 1: This program will simulate a chat bot.
 
+//Variables
+    string answer;
 
+//Give this problem the "Question 1" label.
+    cout << "Question 1.\n";
+    cout << "\n";
+
+//Prompt the user asking if they like the chat bot.
+    cout << "Hello, I'm a C++ chat bot!\n";
+    cout << "Do you like me? Please type yes or no.\n";
+    cin >> answer;
+
+//Determine how the chat bot will respond.
+    if (answer == "yes") {
+        cout << "\nThat's great! I'm sure we'll get along.\n";
+    }
+    else if (answer == "no") {
+        cout << "\nWell, maybe you'll learn to like me later.\n";
+    }
+    else {
+        cout << "\nIf you're not sure... that's OK.\n";
+    }
 
 return 0;
 
