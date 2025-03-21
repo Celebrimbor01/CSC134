440fb838a40ceecfb1e676f5b2e5be5c33c32e39
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-03-21 10:35:13 +0000

    Added the code to draw the 5x5 block of asterisks using a nested loop. M4LAB1 Complete.

--- M4LAB1_Reische.cpp
+++ M4LAB1_Reische.cpp
@@ -3,8 +3,21 @@
 //Dalton Reische
 //3/21/2025
 
-//This program will draw a 5x5 block of asteriks using nested loops
+//This program will draw a 5x5 block of asterisks using nested loops
 
 #include <iostream>
 using namespace std;
 
+int main () {
+
+//Draw the 5x5 block of asterisks using a nested loop
+
+    for (int i = 0; i < 5; i++) 
+    {
+        for (int j = 0; j < 5; j++) { cout << "* "; }
+        cout << endl;
+    }
+
+return 0;
+
+}
