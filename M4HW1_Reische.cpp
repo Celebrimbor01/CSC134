140261b3ab517e6d626a69d3925f356eba83d742
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-03-28 15:56:47 +0000

    Modified the code to add a new while loop that validates the users input is between 1 and 12 before executing the while loop to print the times tables. Part three complete. M4HW1 complete.

--- M4HW1_Reische.cpp
+++ M4HW1_Reische.cpp
@@ -1,5 +1,5 @@
 //CSC 134
-//M4HW1
+//M4HW1 - Gold
 //Dalton Reische
 //3/28/2025
 
@@ -17,6 +17,13 @@ int main () {
 //Prompt the user to enter a number from 1 to 12
     cout << "Please enter a number from 1 to 12: ";
     cin >> user_number;
+
+//While loop to validate the users input
+    while (user_number < 1 || user_number > 12) {
+        cout << "Invalid number.\n"
+             << "Please enter a number from 1 to 12: ";
+        cin >> user_number;
+    }
 
 //While loop to print the 5 times table
     while (number <= 12) {
