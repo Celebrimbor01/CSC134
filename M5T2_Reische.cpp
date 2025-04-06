6f60c32849580a69b1bfac99255682272f83dd8d
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-04-06 11:34:26 +0000

    Started M5T2.

---
+++ M5T2_Reische.cpp
@@ -0,0 +1,15 @@
+//CSC 134
+//M5T2
+//Dalton Reische
+//4/6/2025
+
+#include <iostream>
+using namespace std;
+
+//This program will list the numbers 1 to 10 along with their squares.
+
+int main() {
+
+
+    
+}
