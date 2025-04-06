384ce7027986982324c8c28254d097a98411e504
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-04-06 10:19:40 +0000

    Started M5T1

---
+++ M5T1_Reische.cpp
@@ -0,0 +1,9 @@
+//CSC 134
+//M5T1
+//Dalton Reische
+//4/6/2025
+
+#include <iostream>
+using namespace std;
+
+//Write one void function, and one value returning function, and call them both in main().
