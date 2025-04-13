11ca59ca996951cc06e3b3bc9c8c6e174085768c
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-04-13 07:24:00 +0000

    Started M5LAB2.

---
+++ M5LAB2_Reische.cpp
@@ -0,0 +1,56 @@
+//CSC 134
+//M5LAB2
+//Dalton Reische
+//4/13/2025
+
+#include <iostream>
+using namespace std;
+
+//This program will calculate the area of a rectangle
+
+//Declare functions
+int getLength();
+int getWidth();
+int getArea();
+int displayData();
+
+int main () {
+
+// This program calculates the area of a rectangle.
+// TODO: fix any syntax errors
+	
+    double length;    // The rectangle's length
+    double width;     // The rectangle's width
+    double area;      // The rectangle's area
+   
+// Get the rectangle's length.
+    length = getLength();
+
+// Get the rectangle's width.
+    width = getWidth();
+
+// Get the rectangle's area.
+    area = getArea();
+
+// Display the rectangle's data.
+    displayData(length, width, area);
+
+return 0;
+}
+
+//Define functions
+int getLength(); {
+    
+}
+
+int getWidth(); {
+
+}
+
+int getArea(); {
+
+}
+
+int displayData(); {
+
+}
