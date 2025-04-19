07915db05100d740ed28596fa50ac8acbe17fb00
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-04-19 08:02:57 +0000

    Started M5HW1.

---
+++ M5HW1_Reische.cpp
@@ -0,0 +1,19 @@
+//CSC 134
+//M5HW1
+//Dalton Reische
+//4/19/2025
+
+#include <iostream>
+using namespace std;
+
+//Declare functions
+
+
+
+int main() {
+//Question 1: This program will calculate the average rainfall for three months.
+
+
+
+return 0;
+}
