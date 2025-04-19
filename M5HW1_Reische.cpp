900f3104817e7defb30ef0bd4506f8ee88f50fc5
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-04-19 08:19:34 +0000

    Added the variables and the code to prompt the user for the rain and months using cin to store the inputs into the variables. Added the code to calculate the average rainfall and display it to the user rounded to two decimal places. Question one complete.

--- M5HW1_Reische.cpp
+++ M5HW1_Reische.cpp
@@ -4,6 +4,7 @@
 //4/19/2025
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 //Declare functions
@@ -13,7 +14,38 @@ using namespace std;
 int main() {
 //Question 1: This program will calculate the average rainfall for three months.
 
+//variables
+    string monthOne, monthTwo, monthThree;
+    double rainOne, rainTwo, rainThree;
+    double rainTotal;
+    double rainAverage;
 
+//Give this problem the "Question 1" label.
+    cout << "Question 1.\n";
+
+//Set the output formatting for the numbers.
+    cout << setprecision(2) << fixed;
+
+//Prompt the user for the months and rainfall totals
+    cout << "\nEnter month: ";
+    cin >> monthOne;
+    cout << "Enter rainfall for " << monthOne << ": ";
+    cin >> rainOne;
+    cout << "Enter month: ";
+    cin >> monthTwo;
+    cout << "Enter rainfall for " << monthTwo << ": ";
+    cin >> rainTwo;
+    cout << "Enter month: ";
+    cin >> monthThree;
+    cout << "Enter rainfall for " << monthThree << ": ";
+    cin >> rainThree;
+
+//Calculate the average rainfall for all three months.
+    rainTotal = rainOne + rainTwo + rainThree;
+    rainAverage =  rainTotal / 3.0;
+
+//Display the average rainfall for all three months.
+    cout << "\nThe average rainfall for " << monthOne << ", " << monthTwo << ", and " << monthThree << " is " << rainAverage << " inches.\n";
 
 return 0;
 }
