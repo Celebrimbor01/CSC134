ad710d2f1efa105ba6b3dc1f3774266dfc3db0fd
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2025-05-10 10:17:16 +0000

    Added the code to validate the user's input for both the length and width of the rectangle.

--- M7T2_Reische.cpp
+++ M7T2_Reische.cpp
@@ -67,14 +67,25 @@ double rectLength; //Local variable for length
 //Get the rectangle's width and length from the user.
 
 cout << "This program will calculate the area of a rectangle.\n";
-cout << "\nWhat is the width? ";
-cin >> rectWidth;
-cout << "What is the length? ";
-cin >> rectLength;
 
-//Store the width and the length of the rectangle in the box object.
+//Validate the user's width input.
 
+do{
+    cout << "\nWhat is the width? ";
+    cin >> rectWidth;
+    if (rectWidth <= 0) {
+        cout << "Invalid Width. Width must be greater than zero.\n";
+    }
+} while (rectWidth <= 0);
 box.setWidth(rectWidth);
+
+do{
+    cout << "\nWhat is the length? ";
+    cin >> rectLength;
+    if (rectLength <= 0) {
+        cout << "Invalid Length. Length must be greater than zero.\n";
+    }
+} while (rectLength <= 0);
 box.setLength(rectLength);
 
 //Display the rectangle's data.
